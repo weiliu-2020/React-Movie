713d87d580ebcbe43bff0c624af487f80b8fadb1
Claire Koga <ckoga1@my.bcit.ca>
2020-09-07 10:10:15 -0700

    Added static template image poster

--- movie-app/src/components/MovieGrid.js
+++ movie-app/src/components/MovieGrid.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import tempImg from '../images/avengers-endgame.jpg';
 import { Link } from 'react-router-dom';
 
 class MovieGrid extends React.Component {
     render() {
         return (
         <div class="container">
-            <h2><Link to="/moviepage">Movie Poster</Link></h2>
+            <Link to="/moviepage"><img src= {tempImg} alt="Avengers Endgame Poster" /></Link>
             <p>Movie Title</p>
-            <p>Movie Release Date</p>
+            <p>Jan 1, 1970</p>
         </div>
         );
     }
