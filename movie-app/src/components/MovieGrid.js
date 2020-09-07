ac97eae796d3c7718329a19a511e67f916b86edd
Claire Koga <ckoga1@my.bcit.ca>
2020-09-07 09:53:47 -0700

    Added class to MovieGrid <div>
    This is used for the grid template

--- movie-app/src/components/MovieGrid.js
+++ movie-app/src/components/MovieGrid.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 class MovieGrid extends React.Component {
     render() {
         return (
-        <div>
+        <div class="container">
             <h2><Link to="/moviepage">Movie Poster</Link></h2>
             <p>Movie Title</p>
             <p>Movie Release Date</p>
