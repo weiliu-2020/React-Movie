6d9c76334539e72904413b83dfda36b48e86b746
Claire Koga <ckoga1@my.bcit.ca>
2020-09-16 15:16:14 -0700

    Style MoviePage.js mobile ver.

--- movie-app/src/components/MoviePage.js
+++ movie-app/src/components/MoviePage.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import tempImg from '../images/avengers-endgame.jpg';
+import { Link } from 'react-router-dom';
 
 class MoviePage extends React.Component {
     render() {
         return (
         <main>
             <section>
-                <h2>Movie Poster</h2>
-                <p>Title</p>
-                <p>Release Date</p>
-                <p>Rating</p>
-                <p>Summary: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit porro, dolorem, quod facere enim voluptate provident quo labore vero repellat nemo animi ad exercitationem rem quos, possimus libero deleniti laudantium?</p>
+                <div class="movie-page-container">
+                    <Link to="/moviepage"><img src= {tempImg} alt="Avengers Endgame Poster" /></Link>
+                    <div class="movie-page-info">
+                        <h1>Avengers: Endgame</h1>
+                        <div class="date-rating">
+                            <h3>Jan 1, 1970</h3>
+                            <h3>Rating: 80%</h3>
+                        </div>
+                        <p>Summary: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>  
+                    </div>
+                </div>
             </section> 
         </main>
         );
