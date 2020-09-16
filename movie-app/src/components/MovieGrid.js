bf908f0ff2c21a58a328ccd19be693fad6f0fc14
Hirdey Grewal <grewalhirdey@gmail.com>
2020-09-16 13:51:40 -0700

    Added ratings, changed gradient style and font sizes for movie info

--- movie-app/src/components/MovieGrid.js
+++ movie-app/src/components/MovieGrid.js
@@ -10,16 +10,21 @@ class MovieGrid extends React.Component {
                 <Link to="/moviepage"><img src= {tempImg} alt="Avengers Endgame Poster" /></Link>
                 <div class="movie-info">
                     <h2>Movie Title #1</h2>
-                    <h3>Jan 1, 1970</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                    <div class="ratings">
+                        <h3>Rating:80%</h3>
+                        <h3>Jan 1, 1970</h3>
+                    </div>
+                    <p>After the devastating events of Avengers: Infinity War, the universe is in ruins due to the efforts of the Mad Titan, Thanos. With the help of remaining allies, the Avengers must assemble once more...</p>
                     <div class="button-container"><Link to="/moviepage"><button class="button">More Info</button></Link></div>
                 </div>
             </div>
             <div class="movie movie-02">
                 <Link to="/moviepage"><img src= {tempImg} alt="Avengers Endgame Poster" /></Link>
                 <div class="movie-info">
-                    <h2>Movie Title #1</h2>
-                    <h3>Jan 1, 1970</h3>
+                <div class="ratings">
+                        <h3>Rating:80%</h3>
+                        <h3>Jan 1, 1970</h3>
+                    </div>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
                     <div class="button-container"><Link to="/moviepage"><button class="button">More Info</button></Link></div>
                 </div>        
@@ -27,8 +32,10 @@ class MovieGrid extends React.Component {
             <div class="movie movie-03">
                 <Link to="/moviepage"><img src= {tempImg} alt="Avengers Endgame Poster" /></Link>
                 <div class="movie-info">
-                    <h2>Movie Title #1</h2>
-                    <h3>Jan 1, 1970</h3>
+                <div class="ratings">
+                        <h3>Rating:80%</h3>
+                        <h3>Jan 1, 1970</h3>
+                    </div>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
                     <div class="button-container"><Link to="/moviepage"><button class="button">More Info</button></Link></div>
                 </div>
@@ -36,8 +43,10 @@ class MovieGrid extends React.Component {
             <div class="movie movie-04">
                 <Link to="/moviepage"><img src= {tempImg} alt="Avengers Endgame Poster" /></Link>
                 <div class="movie-info">
-                    <h2>Movie Title #1</h2>
-                    <h3>Jan 1, 1970</h3>
+                <div class="ratings">
+                        <h3>Rating:80%</h3>
+                        <h3>Jan 1, 1970</h3>
+                    </div>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
                     <div class="button-container"><Link to="/moviepage"><button class="button">More Info</button></Link></div>
                 </div>
@@ -45,8 +54,10 @@ class MovieGrid extends React.Component {
             <div class="movie movie-05">
                 <Link to="/moviepage"><img src= {tempImg} alt="Avengers Endgame Poster" /></Link>
                 <div class="movie-info">
-                    <h2>Movie Title #1</h2>
-                    <h3>Jan 1, 1970</h3>
+                <div class="ratings">
+                        <h3>Rating:80%</h3>
+                        <h3>Jan 1, 1970</h3>
+                    </div>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
                     <div class="button-container"><Link to="/moviepage"><button class="button">More Info</button></Link></div>
                 </div>
@@ -54,8 +65,10 @@ class MovieGrid extends React.Component {
             <div class="movie movie-06">
                 <Link to="/moviepage"><img src= {tempImg} alt="Avengers Endgame Poster" /></Link>
                 <div class="movie-info">
-                    <h2>Movie Title #1</h2>
-                    <h3>Jan 1, 1970</h3>
+                <div class="ratings">
+                        <h3>Rating:80%</h3>
+                        <h3>Jan 1, 1970</h3>
+                    </div>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
                     <div class="button-container"><Link to="/moviepage"><button class="button">More Info</button></Link></div>
                 </div>
@@ -63,8 +76,10 @@ class MovieGrid extends React.Component {
             <div class="movie movie-07">
                 <Link to="/moviepage"><img src= {tempImg} alt="Avengers Endgame Poster" /></Link>
                 <div class="movie-info">
-                    <h2>Movie Title #1</h2>
-                    <h3>Jan 1, 1970</h3>
+                <div class="ratings">
+                        <h3>Rating:80%</h3>
+                        <h3>Jan 1, 1970</h3>
+                    </div>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
                     <div class="button-container"><Link to="/moviepage"><button class="button">More Info</button></Link></div>
                 </div>
@@ -72,8 +87,10 @@ class MovieGrid extends React.Component {
             <div class="movie movie-08">
                 <Link to="/moviepage"><img src= {tempImg} alt="Avengers Endgame Poster" /></Link>
                 <div class="movie-info">
-                    <h2>Movie Title #1</h2>
-                    <h3>Jan 1, 1970</h3>
+                <div class="ratings">
+                        <h3>Rating:80%</h3>
+                        <h3>Jan 1, 1970</h3>
+                    </div>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
                     <div class="button-container"><Link to="/moviepage"><button class="button">More Info</button></Link></div>
                 </div>
@@ -81,8 +98,10 @@ class MovieGrid extends React.Component {
             <div class="movie movie-09">
                 <Link to="/moviepage"><img src= {tempImg} alt="Avengers Endgame Poster" /></Link>
                 <div class="movie-info">
-                    <h2>Movie Title #1</h2>
-                    <h3>Jan 1, 1970</h3>
+                <div class="ratings">
+                        <h3>Rating:80%</h3>
+                        <h3>Jan 1, 1970</h3>
+                    </div>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
                     <div class="button-container"><Link to="/moviepage"><button class="button">More Info</button></Link></div>
                 </div>
@@ -90,8 +109,10 @@ class MovieGrid extends React.Component {
             <div class="movie movie-10">
                 <Link to="/moviepage"><img src= {tempImg} alt="Avengers Endgame Poster" /></Link>
                 <div class="movie-info">
-                    <h2>Movie Title #1</h2>
-                    <h3>Jan 1, 1970</h3>
+                <div class="ratings">
+                        <h3>Rating:80%</h3>
+                        <h3>Jan 1, 1970</h3>
+                    </div>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
                     <div class="button-container"><Link to="/moviepage"><button class="button">More Info</button></Link></div>
                 </div>
@@ -99,8 +120,10 @@ class MovieGrid extends React.Component {
             <div class="movie movie-11">
                 <Link to="/moviepage"><img src= {tempImg} alt="Avengers Endgame Poster" /></Link>
                 <div class="movie-info">
-                    <h2>Movie Title #1</h2>
-                    <h3>Jan 1, 1970</h3>
+                <div class="ratings">
+                        <h3>Rating:80%</h3>
+                        <h3>Jan 1, 1970</h3>
+                    </div>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
                     <div class="button-container"><Link to="/moviepage"><button class="button">More Info</button></Link></div>
                 </div>
@@ -108,8 +131,10 @@ class MovieGrid extends React.Component {
             <div class="movie movie-12">
                 <Link to="/moviepage"><img src= {tempImg} alt="Avengers Endgame Poster" /></Link>
                 <div class="movie-info">
-                    <h2>Movie Title #1</h2>
-                    <h3>Jan 1, 1970</h3>
+                <div class="ratings">
+                        <h3>Rating:80%</h3>
+                        <h3>Jan 1, 1970</h3>
+                    </div>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
                     <div class="button-container"><Link to="/moviepage"><button class="button">More Info</button></Link></div>
                 </div>
