a77f71ce91b4cb22c2c505fb36da12d29180ae40
Claire Koga <ckoga1@my.bcit.ca>
2020-09-17 16:21:17 -0700

    Added grid template row

--- movie-app/src/components/MovieGrid.js
+++ movie-app/src/components/MovieGrid.js
@@ -7,9 +7,9 @@ class MovieGrid extends React.Component {
         return (
         <div class="container">
             <div class="movie movie-01">
-                <Link to="/moviepage"><img src= {tempImg} alt="Avengers Endgame Poster" /></Link>
+                <div class="poster-image"><Link to="/moviepage"><img src= {tempImg} alt="Avengers Endgame Poster" /></Link></div>
                 <div class="movie-info">
-                    <h2>Movie Title #1</h2>
+                    <h2>Avengers: Endgame</h2>
                     <div class="ratings">
                         <h3>Rating:80%</h3>
                         <h3>Jan 1, 1970</h3>
@@ -19,9 +19,10 @@ class MovieGrid extends React.Component {
                 </div>
             </div>
             <div class="movie movie-02">
-                <Link to="/moviepage"><img src= {tempImg} alt="Avengers Endgame Poster" /></Link>
+            <div class="poster-image"><Link to="/moviepage"><img src= {tempImg} alt="Avengers Endgame Poster" /></Link></div>
                 <div class="movie-info">
-                <div class="ratings">
+                    <h2>Phineas and Ferb The Movie Candace Against the Universe</h2>    
+                    <div class="ratings">
                         <h3>Rating:80%</h3>
                         <h3>Jan 1, 1970</h3>
                     </div>
@@ -30,20 +31,22 @@ class MovieGrid extends React.Component {
                 </div>        
             </div>
             <div class="movie movie-03">
-                <Link to="/moviepage"><img src= {tempImg} alt="Avengers Endgame Poster" /></Link>
+            <div class="poster-image"><Link to="/moviepage"><img src= {tempImg} alt="Avengers Endgame Poster" /></Link></div>
                 <div class="movie-info">
-                <div class="ratings">
+                    <h2>Avengers: Endgame</h2>
+                    <div class="ratings">
                         <h3>Rating:80%</h3>
                         <h3>Jan 1, 1970</h3>
                     </div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                    <p>After the devastating events of Avengers: Infinity War, the universe is in ruins due to the efforts of the Mad Titan, Thanos. With the help of remaining allies, the Avengers must assemble once more...</p>
                     <div class="button-container"><Link to="/moviepage"><button class="button">More Info</button></Link></div>
                 </div>
             </div>
             <div class="movie movie-04">
-                <Link to="/moviepage"><img src= {tempImg} alt="Avengers Endgame Poster" /></Link>
+            <div class="poster-image"><Link to="/moviepage"><img src= {tempImg} alt="Avengers Endgame Poster" /></Link></div>
                 <div class="movie-info">
-                <div class="ratings">
+                    <h2>Avengers: Endgame</h2>
+                    <div class="ratings">
                         <h3>Rating:80%</h3>
                         <h3>Jan 1, 1970</h3>
                     </div>
@@ -52,9 +55,10 @@ class MovieGrid extends React.Component {
                 </div>
             </div>
             <div class="movie movie-05">
-                <Link to="/moviepage"><img src= {tempImg} alt="Avengers Endgame Poster" /></Link>
+            <div class="poster-image"><Link to="/moviepage"><img src= {tempImg} alt="Avengers Endgame Poster" /></Link></div>
                 <div class="movie-info">
-                <div class="ratings">
+                    <h2>Avengers: Endgame</h2>
+                    <div class="ratings">
                         <h3>Rating:80%</h3>
                         <h3>Jan 1, 1970</h3>
                     </div>
@@ -63,9 +67,10 @@ class MovieGrid extends React.Component {
                 </div>
             </div>
             <div class="movie movie-06">
-                <Link to="/moviepage"><img src= {tempImg} alt="Avengers Endgame Poster" /></Link>
+            <div class="poster-image"><Link to="/moviepage"><img src= {tempImg} alt="Avengers Endgame Poster" /></Link></div>
                 <div class="movie-info">
-                <div class="ratings">
+                    <h2>Avengers: Endgame</h2>
+                    <div class="ratings">
                         <h3>Rating:80%</h3>
                         <h3>Jan 1, 1970</h3>
                     </div>
@@ -74,9 +79,10 @@ class MovieGrid extends React.Component {
                 </div>
             </div>
             <div class="movie movie-07">
-                <Link to="/moviepage"><img src= {tempImg} alt="Avengers Endgame Poster" /></Link>
+            <div class="poster-image"><Link to="/moviepage"><img src= {tempImg} alt="Avengers Endgame Poster" /></Link></div>
                 <div class="movie-info">
-                <div class="ratings">
+                    <h2>Avengers: Endgame</h2>
+                    <div class="ratings">
                         <h3>Rating:80%</h3>
                         <h3>Jan 1, 1970</h3>
                     </div>
@@ -85,9 +91,10 @@ class MovieGrid extends React.Component {
                 </div>
             </div>
             <div class="movie movie-08">
-                <Link to="/moviepage"><img src= {tempImg} alt="Avengers Endgame Poster" /></Link>
+            <div class="poster-image"><Link to="/moviepage"><img src= {tempImg} alt="Avengers Endgame Poster" /></Link></div>
                 <div class="movie-info">
-                <div class="ratings">
+                    <h2>Avengers: Endgame</h2>
+                    <div class="ratings">
                         <h3>Rating:80%</h3>
                         <h3>Jan 1, 1970</h3>
                     </div>
@@ -96,9 +103,10 @@ class MovieGrid extends React.Component {
                 </div>
             </div>
             <div class="movie movie-09">
-                <Link to="/moviepage"><img src= {tempImg} alt="Avengers Endgame Poster" /></Link>
+            <div class="poster-image"><Link to="/moviepage"><img src= {tempImg} alt="Avengers Endgame Poster" /></Link></div>
                 <div class="movie-info">
-                <div class="ratings">
+                    <h2>Avengers: Endgame</h2>
+                    <div class="ratings">
                         <h3>Rating:80%</h3>
                         <h3>Jan 1, 1970</h3>
                     </div>
@@ -107,9 +115,10 @@ class MovieGrid extends React.Component {
                 </div>
             </div>
             <div class="movie movie-10">
-                <Link to="/moviepage"><img src= {tempImg} alt="Avengers Endgame Poster" /></Link>
+            <div class="poster-image"><Link to="/moviepage"><img src= {tempImg} alt="Avengers Endgame Poster" /></Link></div>
                 <div class="movie-info">
-                <div class="ratings">
+                    <h2>Avengers: Endgame</h2>
+                    <div class="ratings">
                         <h3>Rating:80%</h3>
                         <h3>Jan 1, 1970</h3>
                     </div>
@@ -118,9 +127,10 @@ class MovieGrid extends React.Component {
                 </div>
             </div>
             <div class="movie movie-11">
-                <Link to="/moviepage"><img src= {tempImg} alt="Avengers Endgame Poster" /></Link>
+            <div class="poster-image"><Link to="/moviepage"><img src= {tempImg} alt="Avengers Endgame Poster" /></Link></div>
                 <div class="movie-info">
-                <div class="ratings">
+                    <h2>Avengers: Endgame</h2>
+                    <div class="ratings">
                         <h3>Rating:80%</h3>
                         <h3>Jan 1, 1970</h3>
                     </div>
@@ -129,9 +139,10 @@ class MovieGrid extends React.Component {
                 </div>
             </div>
             <div class="movie movie-12">
-                <Link to="/moviepage"><img src= {tempImg} alt="Avengers Endgame Poster" /></Link>
+            <div class="poster-image"><Link to="/moviepage"><img src= {tempImg} alt="Avengers Endgame Poster" /></Link></div>
                 <div class="movie-info">
-                <div class="ratings">
+                    <h2>Avengers: Endgame</h2>
+                    <div class="ratings">
                         <h3>Rating:80%</h3>
                         <h3>Jan 1, 1970</h3>
                     </div>
