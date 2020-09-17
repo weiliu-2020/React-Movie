ed7ecb14c8099519482d7d5903b3c8022807243e
Claire Koga <ckoga1@my.bcit.ca>
2020-09-17 15:03:49 -0700

    Changed white text to beige
    Can be changed later

---
+++ movie-app/src/globals/variables.js
@@ -0,0 +1,8 @@
+// API Variables
+const API_KEY_ONLY            = '7af11c237343537994a3841aef8f4eef';
+export const API_KEY          = `?api_key=${API_KEY_ONLY}`;
+
+// General App Settings
+export const APP_FOLDER_NAME  = '/movie-app';
+
+// LocalStorage Defaults
