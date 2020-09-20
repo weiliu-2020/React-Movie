315c8148c732076b353b3c9417bee5d774cd9058
Claire Koga <ckoga1@my.bcit.ca>
2020-09-19 22:43:47 -0700

    Passed props & isolated data in console log

--- movie-app/src/components/Home.js
+++ movie-app/src/components/Home.js
@@ -2,27 +2,40 @@ import React, {useEffect} from 'react';
 import MovieGrid from './MovieGrid';
 import SearchBar from './SearchBar';
 import TitleFilter from './TitleFilter';
+import {URL_POPULAR, API_KEY} from '../globals/variables';
 
 const Home = () => {
+   
+   const title            = 'Avengers Endgame';
+   const rating           = 'Rating: 80%';
+   let randomArray        = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+   const firstTwelveFilms = randomArray.slice(0,12); 
 
-        useEffect(() => { 
-            const fetchMovie = async () => {
-                const popularResponse = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=7af11c237343537994a3841aef8f4eef&language=en-US&page=1')
-                let data = await popularResponse.json();
-                console.log (data);
+    useEffect(() => { 
+        const fetchMovie = async () => {
+            const popularResponse = await fetch(URL_POPULAR + API_KEY)
+            let data = await popularResponse.json(); 
+            console.log(data);
+            let filmArray = data.results;
+                for (let key in filmArray) {
+                    if (filmArray.hasOwnProperty(key)) {
+                        console.log(filmArray[key].title);
+                        console.log(filmArray[key].vote_average);
+                    }
                 }
-                fetchMovie();
-         }, []);
-
-        return (
+            }
+            fetchMovie();
+        }, []);
+        
+    return (
         <main>
             <section>
                 <SearchBar />
                 <TitleFilter />
-                <MovieGrid />
+                <MovieGrid title={title} rating={rating} anArray={firstTwelveFilms}/>
             </section>
         </main>
-        )
-    };
+    )
+};
 
 export default Home;
--- movie-app/src/components/MovieGrid.js
+++ movie-app/src/components/MovieGrid.js
@@ -1,28 +1,27 @@
 import React from 'react';
 import tempImg from '../images/avengers-endgame.jpg';
-import { Link } from 'react-router-dom';
 import makeDate from '../utilities/dateMaker';
+import { Link } from 'react-router-dom';
 
-const MovieGrid = ({ movie }) => {
+const MovieGrid = (props) => {
 
     const movieDivs = () => {
-        const elements = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
-        return elements.map((index) => {
+        return props.anArray.map((index) => {
             return (
-                <div className={`movie movie-0${index + 1}`} key={index}>
+                <div className={`movie movie-0${index}`} key={index}>
                     <div className="poster-container">
                     <Link className="poster-img" to="/moviepage"><img src= {tempImg} alt="Avengers Endgame Poster" /></Link>
                     <div class="overlay">
                         <div className="movie-desc">
-                            <div className="ratings"><h3>Rating:80%</h3></div>
+                            <div className="ratings"><h3>{props.rating}</h3></div>
                             <p>After the devastating events of Avengers: Infinity War, the universe is in ruins due to the efforts of the Mad Titan, Thanos. With the help of remaining allies, the Avengers must assemble once more...</p>
                             <div className="button-container"><Link to="/moviepage"><button className="button">More Info</button></Link></div>
                         </div>
                     </div>
                     </div>
                     <div className="title-info">
-                        <h2>Avengers: Endgame</h2>
+                        <h2>{props.title}</h2>
                         <h3>{makeDate()}</h3>
                     </div>
                 </div>
@@ -32,7 +31,7 @@ const MovieGrid = ({ movie }) => {
     
     return (
         <div className="container">
-            {movieDivs(movie)}
+            {movieDivs(props)}
         </div>
     )
 };
