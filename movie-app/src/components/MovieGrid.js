32a4c6b1edf0d09b335e85523e68f9d4d5a2b3b6
Wei Liu <liuweileo@gmail.com>
2020-09-22 02:06:10 -0700

    added text fadeout for movie description text

--- movie-app/src/components/MovieGrid.js
+++ movie-app/src/components/MovieGrid.js
@@ -14,7 +14,6 @@ class MovieGrid extends React.Component {
                         <div className="movie-desc">
                             <div class="ratings">
                                 <h3>Rating:80%</h3>
-        <h3>{setDate()}</h3>
                             </div>
                             <p>After the devastating events of Avengers: Infinity War, the universe is in ruins due to the efforts of the Mad Titan, Thanos. With the help of remaining allies, the Avengers must assemble once more...</p>
                             <div class="button-container"><Link to="/moviepage"><button class="button">More Info</button></Link></div>
@@ -34,7 +33,7 @@ class MovieGrid extends React.Component {
                             <div class="ratings">
                                 <h3>Rating:80%</h3>
                             </div>
-                            <p>The work of billionaire tech CEO Donovan Chalmers is so valuable that he hires mercenaries to protect it, and a terrorist group kidnaps his daughter just to get it. </p>
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus, error sapiente asperiores facilis aspernatur autem, labore, beatae distinctio repudiandae ullam adipisci sint voluptatum id sequi fugiat aperiam nobis obcaecati hic officiis! Vel quo dolorem doloribus impedit similique minus optio pariatur laudantium, eius libero ducimus facere praesentium quasi. Natus debitis culpa esse? Repellendus eius dolorem possimus velit optio saepe odio laudantium eligendi voluptatum vitae? Officiis, aliquid fuga numquam assumenda, quaerat earum minus voluptatibus deserunt repellat ex esse autem perspiciatis ducimus. Quas officiis, placeat architecto officia quibusdam ipsa distinctio impedit ut nemo voluptates unde expedita quos itaque amet inventore in porro minima quo perferendis repellat quae ullam earum necessitatibus. Atque, qui quas? Labore optio, inventore numquam molestiae possimus corrupti. Corporis sequi nobis amet! Vel optio facilis, tempore quam nesciunt tenetur! </p>
                             <div class="button-container"><Link to="/moviepage"><button class="button">More Info</button></Link></div>
                         </div>
                     </div>
