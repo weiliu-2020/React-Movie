cf84331c961d9484a52a002605dbdc21af4128fc
Claire Koga <ckoga1@my.bcit.ca>
2020-09-22 16:14:54 -0700

    Passed data to the front page
    Thanks, Michael :)

--- movie-app/src/components/Home.js
+++ movie-app/src/components/Home.js
@@ -1,28 +1,19 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import MovieGrid from './MovieGrid';
 import SearchBar from './SearchBar';
 import TitleFilter from './TitleFilter';
 import {URL_POPULAR, API_KEY} from '../globals/variables';
 
 const Home = () => {
-   
-   const title            = 'Avengers Endgame';
-   const rating           = 'Rating: 80%';
-   let randomArray        = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
-   const firstTwelveFilms = randomArray.slice(0,12); 
+
+    const [movies, setMovies] = useState(null);
 
     useEffect(() => { 
         const fetchMovie = async () => {
             const popularResponse = await fetch(URL_POPULAR + API_KEY)
             let data = await popularResponse.json(); 
-            console.log(data);
             let filmArray = data.results.slice(0,12);
-                for (let key in filmArray) {
-                    if (filmArray.hasOwnProperty(key)) {
-                        console.log(filmArray[key].title);
-                        console.log(filmArray[key].vote_average);
-                    }
-                }
+            setMovies(filmArray) 
             }
             fetchMovie();
         }, []);
@@ -32,7 +23,7 @@ const Home = () => {
             <section>
                 <SearchBar />
                 <TitleFilter />
-                <MovieGrid title={title} rating={rating} anArray={firstTwelveFilms}/>
+                { movies != null && <MovieGrid movies = {movies} />}
             </section>
         </main>
     )
--- movie-app/src/components/MovieGrid.js
+++ movie-app/src/components/MovieGrid.js
@@ -7,22 +7,22 @@ const MovieGrid = (props) => {
 
     const movieDivs = () => {
 
-        return props.anArray.map((index) => {
+        return props.movies.map((movie, i) => {
             return (
-                <div className={`movie movie-0${index}`} key={index}>
+                <div className={`movie movie-0${i}`} key={i}>
                     <div className="poster-container">
-                    <Link className="poster-img" to="/moviepage"><img src= {tempImg} alt="Avengers Endgame Poster" /></Link>
+                    <Link className="poster-img" to="/moviepage"><img src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt="Avengers Endgame Poster" /></Link>
                     <div class="overlay">
                         <div className="movie-desc">
-                            <div className="ratings"><h3>{props.rating}</h3></div>
-                            <p>After the devastating events of Avengers: Infinity War, the universe is in ruins due to the efforts of the Mad Titan, Thanos. With the help of remaining allies, the Avengers must assemble once more...</p>
+                            <div className="ratings"><h3>{movie.vote_average}</h3></div>
+                            <p>{movie.overview}</p>
                             <div className="button-container"><Link to="/moviepage"><button className="button">More Info</button></Link></div>
                         </div>
                     </div>
                     </div>
                     <div className="title-info">
-                        <h2>{props.title}</h2>
-                        <h3>{makeDate()}</h3>
+                        <h2>{movie.title}</h2>
+                        <h3>{movie.release_date}</h3>
                     </div>
                 </div>
             );
