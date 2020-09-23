3de9c34e61ed6a0e6ecbbca06479d3cb7917ca88
Wei Liu <liuweileo@gmail.com>
2020-09-23 14:32:37 -0700

    fixed navbar issue

--- movie-app/src/components/Navbar/Navbar.js
+++ movie-app/src/components/Navbar/Navbar.js
@@ -14,26 +14,27 @@ class Navbar extends Component {
     render() {
         return(
             <header>
-            <h1 className="navbar-logo"><NavLink to="/">Cinemall</NavLink></h1>
-            <nav className="NavbarItems">
-            <div className="menu-icon" onClick={this.handleClick}>
-                <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
-
-            </div>
-            <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
-                {MenuItems.map((item, index) => {
-                    return(
-                        <li key={index}>
-                            <NavLink className={item.cName} to={item.url} >
-                           {item.title} 
-                        </NavLink>
-                        </li>
-                    )
-                })}
-                
-            </ul>
             
-            </nav>
+                <nav className="NavbarItems">
+                    <h1 className="navbar-logo"><NavLink to="/">Cinemall</NavLink></h1>
+                    <div className="menu-icon" onClick={this.handleClick}>
+                        <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+
+                    </div>
+                    <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
+                        {MenuItems.map((item, index) => {
+                            return(
+                                <li key={index}>
+                                    <NavLink className={item.cName} to={item.url} >
+                                {item.title} 
+                                </NavLink>
+                                </li>
+                            )
+                        })}
+                        
+                    </ul>
+                
+                </nav>
             </header>
         )
     }
