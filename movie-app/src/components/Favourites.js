23b2bf5f0f5c86091a0e4b137745efec570f9b62
Claire Koga <ckoga1@my.bcit.ca>
2020-09-27 15:12:39 -0700

    Commented out the array (add back later)

--- movie-app/src/components/Favourites.js
+++ movie-app/src/components/Favourites.js
@@ -4,36 +4,36 @@ import { Link } from 'react-router-dom';
 
 const Favourites = () => {
     const sampleArray = [
-        {   
-            title: 'Avengers: Endgame',
-            release_date: '1970-01-01',
-            vote_average: '8',
-            overview: 'After the devastating events of Avengers: Infinity War, the universe is in ruins due to the efforts of the Mad Titan, Thanos. With the help of remaining allies, the Avengers must assemble once more...',
-            img: {tempImg},
-            id: 24428
-        },
-        {
-            title: 'Random Movie Title',
-            release_date: '1999-12-31',
-            vote_average: '10',
-            overview: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            img: {tempImg},
-            id: 299536
-        },
-        {
-            title: 'A Movie Title',
-            release_date: '2000-01-01',
-            vote_average: '8.3',
-            overview: 'Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium.',
-            img: {tempImg}
-        },
-        {
-            title: 'Another Movie',
-            release_date: '2020-09-27',
-            vote_average: '7.7',
-            overview: 'Bacon ipsum dolor amet short ribs brisket venison rump drumstick pig sausage prosciutto chicken spare ribs salami picanha doner. Kevin capicola sausage, buffalo bresaola venison turkey shoulder picanha ham pork tri-tip meatball meatloaf ribeye. Doner spare ribs andouille bacon sausage. Ground round jerky brisket pastrami shank.',
-            img: {tempImg}
-        }
+        // {   
+        //     title: 'Avengers: Endgame',
+        //     release_date: '1970-01-01',
+        //     vote_average: '8',
+        //     overview: 'After the devastating events of Avengers: Infinity War, the universe is in ruins due to the efforts of the Mad Titan, Thanos. With the help of remaining allies, the Avengers must assemble once more...',
+        //     img: {tempImg},
+        //     id: 24428
+        // },
+        // {
+        //     title: 'Random Movie Title',
+        //     release_date: '1999-12-31',
+        //     vote_average: '10',
+        //     overview: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        //     img: {tempImg},
+        //     id: 299536
+        // },
+        // {
+        //     title: 'A Movie Title',
+        //     release_date: '2000-01-01',
+        //     vote_average: '8.3',
+        //     overview: 'Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium.',
+        //     img: {tempImg}
+        // },
+        // {
+        //     title: 'Another Movie',
+        //     release_date: '2020-09-27',
+        //     vote_average: '7.7',
+        //     overview: 'Bacon ipsum dolor amet short ribs brisket venison rump drumstick pig sausage prosciutto chicken spare ribs salami picanha doner. Kevin capicola sausage, buffalo bresaola venison turkey shoulder picanha ham pork tri-tip meatball meatloaf ribeye. Doner spare ribs andouille bacon sausage. Ground round jerky brisket pastrami shank.',
+        //     img: {tempImg}
+        // }
     ];
 
     function removeClick() {
@@ -44,30 +44,32 @@ const Favourites = () => {
         if (sampleArray && sampleArray.length) {
             return sampleArray.map((fave, i) => {
                 return (
-                    <div className={`movie movie-0${i}`} key={i}>
-                        <div className="poster-container">
-                            <img className="poster-img" src={tempImg} alt={`${fave.title} poster`} />
-                            <div className="overlay">
-                                <div className="movie-desc">
-                                    <div className="ratings"><h3>{fave.vote_average}/10</h3></div>
-                                    <p>{fave.overview}</p>
-                                    <div className="button-container">
-                                        <Link to={`/${fave.id}`}><button className="button more-info">More Info</button></Link>
-                                        <Link to="/favourites"><button id="favourites" className="button" onClick={removeClick}>Remove</button></Link>
+                    <div className="container">
+                        <div className={`movie movie-0${i}`} key={i}>
+                            <div className="poster-container">
+                                <img className="poster-img" src={tempImg} alt={`${fave.title} poster`} />
+                                <div className="overlay">
+                                    <div className="movie-desc">
+                                        <div className="ratings"><h3>{fave.vote_average}/10</h3></div>
+                                        <p>{fave.overview}</p>
+                                        <div className="button-container">
+                                            <Link to={`/${fave.id}`}><button className="button more-info">More Info</button></Link>
+                                            <Link to="/favourites"><button id="favourites" className="button" onClick={removeClick}>Remove</button></Link>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div className="title-info">
-                            <h2>{fave.title}</h2>
-                            <h3>{fave.release_date}</h3>
+                            <div className="title-info">
+                                <h2>{fave.title}</h2>
+                                <h3>{fave.release_date}</h3>
+                            </div>
                         </div>
                     </div>
                 ); 
             });
         } else {
             return (
-                <div className="no-faves">
+                <div className="text-box">
                     <p>No movies have been added to favourites.</p>
                 </div>
             )
@@ -75,7 +77,7 @@ const Favourites = () => {
     }
     
     return (
-        <div className="container">
+        <div className="favourites-container">
             {faveDivs()}
         </div>
     )
